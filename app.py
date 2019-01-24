a484af106ab774468cb10ccf4b0140d2d839e599
babelag <hub.lewandowski@o2.pl>
2019-01-24 22:17:11 +0100

    Adding second layer and layer control

--- app.py
+++ app.py
@@ -16,18 +16,32 @@ def color_producer(elevation):
     else:
         return 'blue'
 
-#map layer (first layer)
+#map layer
 map = folium.Map(location=[38.58, -99.09], zoom_start=4, tiles="Mapbox Bright")
 
-#creating feature group (2 layer)
-fg = folium.FeatureGroup(name="My Map")
+#creating feature group (1 layer)
+fg_volcanos = folium.FeatureGroup(name="My Map")
+
 #iterating
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=str(el)+" m",
+    fg_volcanos.add_child(folium.CircleMarker(location=[lt, ln], radius=8, popup=str(el)+" m",
     fill_color = color_producer(el), color = 'grey', fill_opacity=0.7))
 
+#creating feature group (2 layer)
+fg_population = folium.FeatureGroup(name="Population")
 
-#assigning fg to map
-map.add_child(fg)
+#feature child which is responsible for country borders(poligons) (2 layer)
+fg_population.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+            style_function=lambda x: {"fillColor":"orange" if x["properties"]["POP2005"] < 10000000
+            else "blue" if 10000000 <= x["properties"]["POP2005"] < 200000000 else "red"}))
+
+
+#assigning fg to map, add two layers to the map
+map.add_child(fg_volcanos)
+map.add_child(fg_population)
+
+#adding layer control (must be added after assigning map adding feature group
+map.add_child(folium.LayerControl(position="topright", collapsed=True, autoZIndex=True))
+
 #saving data to the file
 map.save("Map1.html")
